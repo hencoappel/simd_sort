843b6b40aec7738211b8a449ce9b418a3e839336
Henco Appel <hencoappel@gmail.com>
2020-02-15 11:22:23 +0000

    Improve the benchmarks
    Make the benchmarks parameterised with different length arrays to see
    the trends in the performance.

--- benches/sort_bench.rs
+++ benches/sort_bench.rs
@@ -1,32 +1,43 @@
 #[macro_use]
 extern crate criterion;
 
-use simd_sort::quick_sort;
+use criterion::BenchmarkId;
 use criterion::Criterion;
+use rand::{Rng, thread_rng};
 
+use simd_sort::quick_sort;
 
 fn rust_sort_benchmark(c: &mut Criterion) {
-    c.bench_function("Rust sort", |b| b.iter(|| {
-        let mut data = vec![5, 2, 1, 4, 3];
-        data.sort_unstable();
-    }));
-}
-fn rust_unstable_sort_benchmark(c: &mut Criterion) {
-    c.bench_function("Rust unstable_sort", |b| b.iter(|| {
-        let mut data = vec![5, 2, 1, 4, 3];
-        data.sort_unstable();
-    }));
+    bench_sort_func(c, "Rust sort", |v| v.sort());
 }
 
+fn rust_unstable_sort_benchmark(c: &mut Criterion) {
+    bench_sort_func(c, "Rust sort unstable", |v| v.sort_unstable());
+}
 
 fn quicksort_benchmark(c: &mut Criterion) {
-    c.bench_function("quicksort", |b| b.iter(|| {
-        let mut data = vec![5, 2, 1, 4, 3];
-        quick_sort(&mut data);
-    }));
+    bench_sort_func(c, "quicksort", quick_sort);
 }
 
-criterion_group!(benches,
-                 rust_sort_benchmark, rust_unstable_sort_benchmark,
-                 quicksort_benchmark);
+fn bench_sort_func<F>(c: &mut Criterion, group_name: &str, mut f: F) where F: FnMut(&mut [i64]) {
+    let mut group = c.benchmark_group(group_name);
+    for n in (1..4).map(|i| 10_usize.pow(i)) {
+        group.bench_with_input(BenchmarkId::from_parameter(n), &n, |b, &n| {
+            b.iter(|| {
+                f(&mut random_array(n));
+            });
+        });
+    }
+}
+
+fn random_array(n: usize) -> Vec<i64>{
+    thread_rng().gen_iter::<i64>().take(n).collect()
+}
+
+criterion_group!(
+    benches,
+    rust_sort_benchmark,
+    rust_unstable_sort_benchmark,
+    quicksort_benchmark
+);
 criterion_main!(benches);
